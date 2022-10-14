3877762dba37354ad9379015e85ced5a92888d2e
Snehit Sah <snehitsah@protonmail.com>
2022-10-14 10:51:41 +0530

    Format
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- tgfs.py
+++ tgfs.py
@@ -1,8 +1,10 @@
 import sys
 import sqlite3
 
+
 def add_tag_to_file(tag_name, list_of_files):
     print("add tag to file function")
+
 
 def create_tag(tag_name):
     print("created tag to file function")
@@ -12,8 +14,6 @@ def remove_tag_from_file(tag_name, list_of_files):
 
 def delete_tag(tag_name):
     print("delete tag")
-
-
 
 
 if __name__ == "__main__":
@@ -27,5 +27,4 @@ if __name__ == "__main__":
     elif sys.argv[1] == "remove":
         remove_tag_from_file( sys.argv[2],sys.argv[3:])
     elif sys.argv[1] == "delete":
-        delete_tag( sys.argv[2])
-        
+        delete_tag(sys.argv[2])
