360f58d76ab580bedd61cc521f7addda08b2e421
Snehit Sah <snehitsah@protonmail.com>
2022-10-14 12:05:35 +0530

    Lint
    Signed-off-by: Snehit Sah <snehitsah@protonmail.com>

--- tgfs.py
+++ tgfs.py
@@ -1,21 +1,31 @@
+""""Tag Based File System"""
+
 import sys
 import sqlite3
 
 
 def add_tag_to_file(tag_name, list_of_files: list[str]):
+    """"Add a tag to a list of files"""
     print("add tag to file function")
 
 
 def create_tag(tag_name: str):
+    """Create a new tag"""
     print("created tag to file function")
 
+
 def remove_tag_from_file(tag_name: str, list_of_files: list[str]):
+    """"Remove a tag from list of files"""
     print("removed tag to file function")
 
+
 def delete_tag(tag_name: str):
+    """"Delete a tag"""
     print("delete tag")
 
-def init_db(sqliteConnection: sqlite3.Connection):
+
+def init_db(sqlite_connection: sqlite3.Connection):
+    """"Initialize the tgfs database"""
     init_db_query = '''CREATE TABLE tags
 (
   name VARCHAR(256) NOT NULL,
@@ -28,21 +38,22 @@ CREATE TABLE file_list
   tag_name VARCHAR(256) NOT NULL,
   PRIMARY KEY (file_name, tag_name)
 );'''
-    sqliteConnection.cursor().execute(init_db_query)
-    sqliteConnection.commit()
+    sqlite_connection.cursor().execute(init_db_query)
+    sqlite_connection.commit()
     print("Database initialized")
 
 
-if __name__ == "__main__":
+def main():
+    """The entrypoint"""
 
     print("we are in main function")
 
-    sqliteConnection = sqlite3.connect('~/.tgfs.db')
+    sqlite_connection = sqlite3.connect('~/.tgfs.db')
 
     subcommand = sys.argv[1]
 
     if subcommand == "add":
-        add_tag_to_file(sys.argv[2],sys.argv[3:]) 
+        add_tag_to_file(sys.argv[2],sys.argv[3:])
     elif subcommand == "create":
         create_tag( sys.argv[2])
     elif subcommand == "remove":
@@ -50,9 +61,13 @@ if __name__ == "__main__":
     elif subcommand == "delete":
         delete_tag(sys.argv[2])
     elif subcommand == "init":
-        init_db(sqliteConnection)
+        init_db(sqlite_connection)
     else:
         print("Incorrect command format")
-    
-    if sqliteConnection:
-        sqliteConnection.close()
+
+    if sqlite_connection:
+        sqlite_connection.close()
+
+
+if __name__ == "__main__":
+    main()
